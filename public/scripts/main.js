a63388a13f58c8f01bdcf0546aebe13b69110c49
Jones <jonescl1@rose-hulman.edu>
2022-10-28 10:51:10 -0400

    created FAB, added placeholder functionality to FAB, skeletons for list, edit, and calendar pages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,34 +3,76 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * PUT_YOUR_NAME_HERE
+ * Corwin Jones, Allison Abernathie, Larissa Krakora
  */
 
 /** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+function htmlToElement(html) {
+	var template = document.createElement('template');
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
+rhit.editController = class {
+	constructor(){
+		console.log("edit mode");
+	}
+};
+rhit.calendarController = class {
+	constructor(){
+		console.log("calendar mode");
+	}
+};
+rhit.listController = class {
+	constructor(){
+		console.log("list mode");
+		document.querySelector("#fab").addEventListener("click", (event) =>{
+			this.updateList();
+		});
+	}
+
+	updateList(){
+		const newCheck = this._createElement("place holder");
+
+		const checklist = document.querySelector("#checklist");
+		checklist.appendChild(newCheck);
+	};
+
+	_createElement(todoItem){
+		return htmlToElement(`<div class="card">
+		<div class="card-body">
+		  <div class="form-check">
+			<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+			<label class="form-check-label" for="defaultCheck1">
+			  ${todoItem}
+			</label>
+		  </div>
+		</div>
+	  </div>`)
+	}
 };
 
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	if(document.querySelector("#editPage")){
+		
+		new rhit.editController();
+	}
+	if(document.querySelector("#calendarPage")){
+		
+		new rhit.calendarController();
+	}
+	if(document.querySelector("#listPage")){
+		
+		new rhit.listController();
+	}
+
 };
 
 rhit.main();
