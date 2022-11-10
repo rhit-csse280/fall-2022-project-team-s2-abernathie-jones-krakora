c2bed55db2bb065e299d8a5ffd1cc5a23a87bd9c
krakorlr <krakorlr@rose-hulman.edu>
2022-11-10 14:56:14 -0500

    updatelist

--- public/scripts/calendarPage.js
+++ public/scripts/calendarPage.js
@@ -33,8 +33,35 @@ class CalendarController {
 		});
 		updateList();
 	};
-	updateList(){
+	
+	updateList() {
+		const newList = htmlToElement('<div id="calendarPage" class="container page-container"></div>');
 
+		for(let i = 0; i < rhit.fbMultiAssManager.length; i++) {
+			const ass = rhit.fbMultiAssManager.getAssAtIndex(i);
+			const newCard = this._createCard(ass);
+
+			newList.appendChild(newCard);
+		}
+
+		const oldList = document.querySelector("#dayList");
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+
+		oldList.parentElement.appendChild(newList);
+	}
+
+	_createCard(todoItem) {
+		return htmlToElement(`<div class="card">
+		<div class="card-body">
+			<div class="form-check">
+				<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+				<label class="form-check-label" for="defaultCheck1">
+					${todoItem}
+				</label>
+			</div>
+		</div>
+	</div>`);
 	}
 }
 
