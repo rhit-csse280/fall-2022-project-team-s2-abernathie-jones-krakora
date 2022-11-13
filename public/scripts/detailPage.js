033c1d5be8f68dd6748e24a6f2c8a13ac0c8dc5f
krakorlr <krakorlr@rose-hulman.edu>
2022-11-12 19:24:52 -0500

    signout wip

--- public/scripts/detailPage.js
+++ public/scripts/detailPage.js
@@ -18,6 +18,10 @@ class DetailController {
         const fbAssManager = ass;
 		console.log("detail mode");
 
+        // Sidebar Navigation
+        document.querySelector("#menuSignOut").addEventListener("click", (event) => {
+			rhit.fbAuthManager.signOut();
+		});
         const listItem = document.querySelector("#goToListPage");
         if(listItem) {
             document.querySelector("#goToListPage").addEventListener("click", (event) => {
@@ -30,9 +34,38 @@ class DetailController {
             document.querySelector("#goToCalendarPage").addEventListener("click", (event) => {
                 window.location.href = "/calendar.html";
                 console.log("Go to calendar");
-            });
-            
+            });  
         }
+
+        //Edit an Assignment
+        document.querySelector("#submitEditAss").addEventListener("click", (event) => {
+			const name = document.querySelector("#inputName").value;
+			const subject = document.querySelector("#inputClass").value;
+            const date = document.querySelector("#inputDate").value;
+            const priority = document.querySelector("#inputPriority").checked;
+
+			rhit.fbSingleAssManager.update(name, subject, date, priority);
+		});
+        $("#editAssDialog").on("show.bs.modal", (event) => {
+			console.log("dialog about to show up");
+			document.querySelector("#inputName").value = rhit.fbSingleAssManager.name;
+			document.querySelector("#inputClass").value = rhit.fbSingleAssManager.class;
+            document.querySelector("#inputDate").value = rhit.fbSingleAssManager.date;
+            document.querySelector("#inputPriority").checked = rhit.fbSingleAssManager.priority;
+		});
+		$("#editAssDialog").on("shown.bs.modal", (event) => {
+			console.log("dialog is now visible");
+			document.querySelector("#inputName").focus();
+		});
+        document.querySelector("#submitDeleteAss").addEventListener("click", (event) => {
+			rhit.fbSingleAssManager.delete().then(function() {
+				console.log("Document successfully deleted!");
+			}).catch(function (error) {
+				console.error("Error removing document: ", error);
+			});
+		});
+
+
 		//updateList();
 	}
 	updateList() {
@@ -40,6 +73,11 @@ class DetailController {
 		for(let i = 0; i < rhit.fbMultiAssManager.length; i++) {
 			const ass = rhit.fbMultiAssManager.getAssAtIndex(i);
 			const newCard = this._createCard(ass);
+
+            newCard.onclick = (event) => {
+                ass.id
+            }
+
 			newList.appendChild(newCard);
 		}
 		const oldList = document.querySelector("#dayList");
@@ -47,7 +85,7 @@ class DetailController {
 		oldList.remove();
 	}
 	_createCard(todoItem) {
-		return _htmlToElement(`<div class="card">
+		return _htmlToElement(`<div class="card" name=${todoItem.id} data-toggle="modal" data-target="#editAssDialog">
 		<div class="card-body">
 			<div class="form-check">
 				<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
